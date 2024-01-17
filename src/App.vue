120669a594af10fb60a9a29118c725b34c2fccbb
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-17 16:49:37 +0100

    correzione footer

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 import AppFooter from './components/AppFooter.vue'
-export default{
-  components:{
+export default {
+  components: {
     AppHeader,
     AppMain,
-    AppFooter
+    AppFooter,
   }
 }
 </script>
@@ -19,15 +19,18 @@ export default{
 
 <style lang="scss">
 @use "./styles/generals.scss" as *;
+
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,11 @@
 <script>
 import { store } from "../store.js"
-export default{
-    data(){
-      return{
-        store
-      }
+export default {
+  data() {
+    return {
+      store
     }
+  }
 }
 </script>
 <template lang="">
@@ -15,9 +15,10 @@ export default{
 </template>
 <style lang ="scss" scoped>
 @use "./../styles/generals.scss" as *;
-.sfondo.footer{
-    background-image: url("/footer-bg-football.jpg");
-    height: 300px;
-    background-color: black;
+
+.sfondo-footer {
+  background-image: url("/footer-bg-football.jpg");
+  height: 300px;
+  background-color: black;
 }
 </style>
