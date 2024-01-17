c17f85067c9c83b766410864cccd1e2600c18ea0
Shamble94 <fratus.nicholas@gmail.com>
2024-01-17 16:11:14 +0100

    Branch header

---
+++ src/store.js
@@ -0,0 +1,26 @@
+import { reactive } from "vue";
+export const store = reactive ({
+   navbar:[
+    {
+        link: "home"
+    },
+    {
+        link: "fixtures & results"
+    },
+    {
+        link: "league table"
+    },
+    {
+        link: "players"
+    },
+    {
+        link: "gallery"
+    },
+    {
+        link: "blog"
+    },
+    {
+        link: "contact"
+    },
+   ]
+})
