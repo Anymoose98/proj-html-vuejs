13c08875c559d1fa165ba13e217088aa01f1a6cc
Anymoose98 <daniel.digiacomo98@gmail.com>
2024-01-18 12:58:22 +0100

    store creato

--- src/store.js
+++ src/store.js
@@ -1,52 +1,91 @@
 import { reactive } from "vue";
-export const store = reactive ({
-   navbar:[
-    {
-        link: "home"
-    },
-    {
-        link: "fixtures & results"
-    },
-    {
-        link: "league table"
-    },
-    {
-        link: "players"
-    },
-    {
-        link: "gallery"
-    },
-    {
-        link: "blog"
-    },
-    {
-        link: "contact"
-    },
-   ],
-   players:[
-    {
-        name:"Adam Brown",
-        image: "/player1.jpg",
-        role: "Forwarder",
-        number: "07"
-    },
-    {
-        name: "Michael Kayn",
-        image: "/player2.jpg",
-        role: "Forwarder",
-        number: "09"
-    },
-    {
-        name: "Micheal Lee",
-        image: "/player3.jpg",
-        role: "Forwarder",
-        number: "11"
-    },
-    {
-        name: "Ethan Smith",
-        image: "/player4.jpg",
-        role: "Midfielder",
-        number: 15
-    }
-   ]
+export const store = reactive({
+    navbar: [
+        {
+            link: "home"
+        },
+        {
+            link: "fixtures & results"
+        },
+        {
+            link: "league table"
+        },
+        {
+            link: "players"
+        },
+        {
+            link: "gallery"
+        },
+        {
+            link: "blog"
+        },
+        {
+            link: "contact"
+        },
+    ],
+    players: [
+        {
+            name: "Adam Brown",
+            image: "/player1.jpg",
+            role: "Forwarder",
+            number: "07"
+        },
+        {
+            name: "Michael Kayn",
+            image: "/player2.jpg",
+            role: "Forwarder",
+            number: "09"
+        },
+        {
+            name: "Micheal Lee",
+            image: "/player3.jpg",
+            role: "Forwarder",
+            number: "11"
+        },
+        {
+            name: "Ethan Smith",
+            image: "/player4.jpg",
+            role: "Midfielder",
+            number: 15
+        }
+    ],
+
+    team: [
+        {
+            name: " Instambul Sports",
+            img: "/league-club-1-png"
+        },
+        {
+            name: " United Fs Club",
+            img: "/league-club-2-png"
+        },
+        {
+            name: "Schalke Club",
+            img: "/league-club-3-png"
+        },
+        {
+            name: "Inter Club",
+            img: "/league-club-4-png"
+        },
+        {
+            name: "Madrid Club",
+            img: "/league-club-5-png"
+        },
+        {
+            name: "Schalke Club",
+            img: "/league-club-6-png"
+        },
+        {
+            name: "Arsenal Club",
+            img: "/league-club-7-png"
+        },
+        {
+            name: "Bayern Club",
+            img: "/league-club-8-png"
+        },
+        {
+            name: "Atletico Club",
+            img: "/leage-club-9.png"
+        }
+    ],
 })
